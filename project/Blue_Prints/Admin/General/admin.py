0247cf1009496cb8cd560e8d2de003c52cc37912
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-02-21 19:06:57 +0100

    M

--- project/Blue_Prints/Admin/General/admin.py
+++ project/Blue_Prints/Admin/General/admin.py
@@ -1,19 +1,21 @@
 from flask import Blueprint, render_template, request, redirect, flash
-from ... import db
 from ....models.Therapist import Therapist
 from ....models.User import User
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-admin = Blueprint('admin', __name__, template_folder='templates', static_folder="static", static_url_path='/Admin/General/static')
-
-@admin.route('admin/home')
-def medic_appointments():
-    return render_template('/medic/appointments.html')
+admin = Blueprint('admin', __name__, template_folder='templates', static_folder="static", static_url_path='/Blue_Prints/Admin/General/static')
 
 @admin.route('/admin/home')
 def admin_home():
+    """
+        This endpoint renders admin homepage.
+    """
     therapists = Therapist.query.all()
     users = User.query.all()
-    return render_template('admin/home.html', therapists=therapists, users=users)
+    return render_template('admin/home/home.html', therapists=therapists, users=users)
+
+@admin.route('/admin/locations')
+def admin_locations():
+    return render_template('admin/locations/index.html')
